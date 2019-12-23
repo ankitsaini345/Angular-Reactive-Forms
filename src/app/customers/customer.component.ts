48546832a5c3b8cb4bdc16faea301bbaf5b4d131
ankitsaini345 <ankitsaini345@gmail.com>
2019-12-23 13:09:30 +0530

    formControl changed to formBuilder

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,39 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms'
-import { Customer } from './customer';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { Customer } from "./customer";
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  selector: "app-customer",
+  templateUrl: "./customer.component.html",
+  styleUrls: ["./customer.component.css"]
 })
 export class CustomerComponent implements OnInit {
-  customerForm: FormGroup
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendcatalog : new FormControl(true)
-
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: {value: 'disabled', disabled: true},
+      email: '',
+      sendcatalog: true
     });
   }
 
   populateTestData() {
     this.customerForm.setValue({
-    firstName: 'first',
-    lastName: 'last',
-    email: 'test@gmail.com',
-    sendcatalog: false
-    })
+      firstName: "first",
+      lastName: "last",
+      email: "test@gmail.com",
+      sendcatalog: false
+    });
   }
 
   save() {
     console.log(this.customerForm.form);
-    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    console.log("Saved: " + JSON.stringify(this.customerForm.value));
   }
 }
